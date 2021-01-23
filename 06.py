0e309b5ab69c845fa0256f8794aca854650cde2c
marin1401 <58388551+marin1401@users.noreply.github.com>
2021-01-23 14:39:55 +0100

    Update 06.py

--- 06.py
+++ 06.py
@@ -3,23 +3,24 @@
 with open('./06.txt') as myinput:
     coords_list = myinput.readlines()
 
-coords = {str(i): tuple(map(int, (coord.strip().split(', ')))) for i, coord in enumerate(coords_list)}
+coords = {i: tuple(map(int, (coord.strip().split(', ')))) for i, coord in enumerate(coords_list)}
 
-x_coords = sorted(coord[0] for coord in coords.values())
-y_coords = sorted(coord[1] for coord in coords.values())
+x_min, *_, x_max = sorted(coord[0] for coord in coords.values())
+y_min, *_, y_max = sorted(coord[1] for coord in coords.values())
 
-x_range = [x for x in range(x_coords[0], x_coords[-1] + 1)]
-y_range = [y for y in range(y_coords[0], y_coords[-1] + 1)]
+x_range = range(x_min, x_max + 1)
+y_range = range(y_min, y_max + 1)
 
 def get_closest_point(x, y, coords):
-    (distance_1, point_1), (distance_2, point_2) = (sorted((abs(x - xc) + abs(y - yc), point) for point, (xc, yc) in coords.items())[i] for i in range(2))
-    return point_1 if distance_1 != distance_2 else ''
+    (distance_1, point_1), (distance_2, point_2), *_ = sorted((abs(x - xc) + abs(y - yc), point) for point, (xc, yc) in coords.items())
+    if distance_1 != distance_2:
+        return point_1
 
 grid = [[get_closest_point(x, y, coords) for x in x_range] for y in y_range]
 
-points_closest_to_border = [line[0] for line in grid] + [line[-1] for line in grid] + grid[0] + grid[-1]
+points_closest_to_border = set([line[0] for line in grid] + [line[-1] for line in grid] + grid[0] + grid[-1])
 
-valid_points = [point for point in coords.keys() if point not in set(points_closest_to_border)]
+valid_points = [point for point in coords.keys() if point not in points_closest_to_border]
 
 #Part 1
 
